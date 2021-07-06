f46fa5aa721c2b7b99eb68d062f5832dbfae605a
ArtemAA1990 <a.avramenko.a@mail.ru>
2021-07-06 13:13:02 +0300

    Initial Commit

---
+++ squareColour/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  squareColour
+//
+//  Created by Artem Avramenko on 06.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
